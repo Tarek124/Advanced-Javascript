5cdae566da301f9d57c7c2a960c1847a0212ef47
tarek124 <afrantarek123456789@gmail.com>
2023-07-09 09:47:18 +0600

    true and false

---
+++ true-false.js
@@ -0,0 +1,34 @@
+const age = 4;
+
+if (age) {
+    console.log("condition is true")
+
+}
+else {
+    console.log("condition is false")
+}
+
+
+const name = "Solaiman";
+
+if (name.length > 0) {
+    console.log("condition is true")
+
+}
+else {
+    console.log("condition is false")
+}
+
+// false{
+//    0
+//    ""
+//    undenfined
+//    null
+//    NAN
+// }
+
+// true{
+"0"
+" "
+"[]"
+//}
