2f1ac5cb33719307424f9c7d7749501c74488225
tarek124 <afrantarek123456789@gmail.com>
2023-08-08 10:07:13 +0600

    for in for of done

---
+++ forIn.js
@@ -0,0 +1,15 @@
+const myObj = {
+    name: "javascript",
+    estd: '2323',
+    founder: "Brendan Eich "
+},
+    myAry = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (subTotal of myAry) {
+    console.log(subTotal)
+
+}
+
+
+// for (property in myObj) {
+//     console.log(property);
+// }
